3bf3778a2c7089ecfb07824a688d8c68d62cbfb1
Thiago Carvalho <carvalho@pythian.com>
2016-08-16 20:38:10 +0000

    fixs start service path

--- manifests/cluster/startservice.pp
+++ manifests/cluster/startservice.pp
@@ -38,7 +38,7 @@ define cloudera::cluster::startservice (
 ) {
 
   exec { "start service $hadoop_service_start":
-    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v1/clusters/$cdh_cluster_name/services/$hadoop_service_start/start\" -d @$hadoop_service_start-roles.json && touch /var/tmp/$hadoop_service_start-started.lock",
+    command => "/bin/echo /usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v1/clusters/$cdh_cluster_name/services/$hadoop_service_start/command/start\" && touch /var/tmp/$hadoop_service_start-started.lock",
     cwd     => "/tmp",
     creates => "/var/tmp/$hadoop_service_start-started.lock",
     tries   => 3,
