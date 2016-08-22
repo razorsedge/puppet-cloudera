f031d11d37004388a38ac44b8f6ff10749b4804f
Thiago Carvalho <carvalho@pythian.com>
2016-08-22 22:08:47 +0000

    change start service to firstRUn

--- manifests/cluster/startservice.pp
+++ manifests/cluster/startservice.pp
@@ -28,18 +28,19 @@
 #
 
 define cloudera::cluster::startservice (
+  $cdh_metadata_dir  = $cloudera::params::cdh_metadata_dir,
   $cdh_cluster_name  = $cloudera::params::cdh_cluster_name,
   $cm_api_host       = $cloudera::params::cm_api_host,
   $cm_api_port       = $cloudera::params::cm_api_port,
   $cm_api_user       = $cloudera::params::cm_api_user,
   $cm_api_password   = $cloudera::params::cm_api_password,
-  $hadoop_service_start = $title,
+  $cdh_cluster_service = $title,
 ) {
 
-  exec { "start service $hadoop_service_start":
-    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v1/clusters/$cdh_cluster_name/services/$hadoop_service_start/commands/start\" && touch /var/tmp/$hadoop_service_start-started.lock",
+  exec { "start service $cdh_cluster_service":
+    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/services/$cdh_cluster_service/commands/firstRun\" > $cdh_metadata_dir/$cdh_cluster_service-started.lock",
     cwd     => "/tmp",
-    creates => "/var/tmp/$hadoop_service_start-started.lock",
+    creates => "$cdh_metadata_dir/$cdh_cluster_service-started.lock",
     tries   => 3,
     try_sleep => 60
   }
