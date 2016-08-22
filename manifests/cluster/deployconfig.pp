b5d3b4de6c79dcb48f6835416da5d5c555d0ac03
Thiago Nache B. de Carvalho <carvalho@pythian.com>
2016-08-22 16:37:00 -0300

    using new API version

--- manifests/cluster/deployconfig.pp
+++ manifests/cluster/deployconfig.pp
@@ -29,18 +29,15 @@
 
 define cloudera::cluster::deployconfig (
   $cdh_cluster_name  = $cloudera::params::cdh_cluster_name,
-  $cdh_role_name     = $title,
   $cm_api_host       = $cloudera::params::cm_api_host,
   $cm_api_port       = $cloudera::params::cm_api_port,
   $cm_api_user       = $cloudera::params::cm_api_user,
-  $cm_api_password   = $cloudera::params::cm_api_password,
-  $hadoop_service_deploy_config = $hadoop_service_deploy_config
+  $cm_api_password   = $cloudera::params::cm_api_password
 ) {
 
   exec { "deploy config service $cdh_role_name":
-    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v1/clusters/$cdh_cluster_name/services/$hadoop_service_deploy_config/commands/deployClientConfig\" -d '{ \"items\": [\"$cdh_role_name\"] }' && touch /var/tmp/$cdh_role_name-client-configured.lock",
+    command => "/usr/bin/curl -H 'Content-Type: application/json' -u $cloudera::params::cm_api_user:$cloudera::params::cm_api_password -XPOST \"http://$cm_api_host:$cm_api_port/api/v13/clusters/$cdh_cluster_name/commands/deployClientConfig\" -d '{ }'",
     cwd     => "/tmp",
-    creates => "/var/tmp/$cdh_role_name-client-configured.lock",
     tries   => 3,
     try_sleep => 60
   }
